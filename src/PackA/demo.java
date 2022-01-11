ad6342752dfacad2b63bbb27bd344298ad4eb8fb
Rajaram240 <rajaramk683@gmail.com>
2022-01-11 17:48:42 +0530

    first commit

---
+++ src/PackA/demo.java
@@ -0,0 +1,18 @@
+package PackA;
+
+import org.apache.logging.log4j.*;
+
+public class demo {
+   private static Logger log=LogManager.getLogger(demo.class.getName());
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+log.debug("I am debugging");
+
+	log.info("object is present");
+
+	log.error("object is not present");
+	log.fatal("fatal");
+
+	}
+
+}
